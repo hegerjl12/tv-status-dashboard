1dc8f0dadc30a23c5f3bd9e0ed55cef20b2bd17c
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-01-27 10:08:46 -0800

    all metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,18 @@ spy30m_db = deta.Base('SPY30m')
 spy1h_db = deta.Base('SPY1h')
 
 spy1m_data = spy1m_db.get('current')
-st.metric(label='SPY 1m', value=spy1m_data['signal'], delta=spy1m_data['price'])
+spy3m_data = spy3m_db.get('current')
+spy5m_data = spy5m_db.get('current')
+spy15m_data = spy15m_db.get('current')
+spy30m_data = spy30m_db.get('current')
+spy1h_data = spy1h_db.get('curren
+
+                         
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+                          
+col1.metric(label='SPY 1m', value=spy1m_data['signal'], delta=spy1m_data['price'])
+col2.metric(label='SPY 3m', value=spy3m_data['signal'], delta=spy3m_data['price'])
+col3.metric(label='SPY 5m', value=spy5m_data['signal'], delta=spy5m_data['price'])                          
+col4.metric(label='SPY 15m', value=spy15m_data['signal'], delta=spy15m_data['price'])                          
+col5.metric(label='SPY 30m', value=spy30m_data['signal'], delta=spy30m_data['price'])                          
+col6.metric(label='SPY 1h', value=spy1h_data['signal'], delta=spy1h_data['price'])                          
