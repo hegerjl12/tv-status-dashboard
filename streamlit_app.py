97440fafa2be938b2965bff06854cddb7c5187fa
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-01-27 09:43:46 -0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from deta import Deta
+
+deta = Deta('b0hip04s_DyG5HST9fRrAtbUr425Q9bDNLSaLScv5')
+
+spy1m_db = deta.Base('SPY1m')
+spy3m_db = deta.Base('SPY3m')
+spy5m_db = deta.Base('SPY5m')
+spy15m_db = deta.Base('SPY15m')
+spy30m_db = deta.Base('SPY30m')
+spy1h_db = deta.Base('SPY1h')
+
+st.metric(spy1m_db.get('current'))
