4111d2840afc7961d8249f61e7c10b08d4aa7634
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-01-27 10:02:56 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,5 @@ spy15m_db = deta.Base('SPY15m')
 spy30m_db = deta.Base('SPY30m')
 spy1h_db = deta.Base('SPY1h')
 
-st.metric(label='SPY 1m', value=spy1m_db.get('current'))
+spy1m_data = spy1m_db.get('current')
+st.metric(label='SPY 1m', value=spy1m_data['signal'], delta=spy1m_data['price'])
