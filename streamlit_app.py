a41ac7c3c32fa2d8b41d424894b6a03486151724
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-01-30 09:33:57 -0800

    uncomment all timeframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,9 @@ if spy1m_data['signal'] == 'sell':
 
 spy3m_data = spy3m_db.get('current')
 spy5m_data = spy5m_db.get('current')
-#spy15m_data = spy15m_db.get('current')
-#spy30m_data = spy30m_db.get('current')
-#spy1h_data = spy1h_db.get('current')
+spy15m_data = spy15m_db.get('current')
+spy30m_data = spy30m_db.get('current')
+spy1h_data = spy1h_db.get('current')
 
                          
 col1, col2, col3, col4, col5, col6 = st.columns(6)
@@ -27,6 +27,6 @@ col1, col2, col3, col4, col5, col6 = st.columns(6)
 col1.metric(label='SPY 1m', value=spy1m_data['signal'], delta=spy1m_price)
 col2.metric(label='SPY 3m', value=spy3m_data['signal'], delta=spy3m_data['price'])
 col3.metric(label='SPY 5m', value=spy5m_data['signal'], delta=spy5m_data['price'])                          
-#col4.metric(label='SPY 15m', value=spy15m_data['signal'], delta=spy15m_data['price'])                          
-#col5.metric(label='SPY 30m', value=spy30m_data['signal'], delta=spy30m_data['price'])                          
-#col6.metric(label='SPY 1h', value=spy1h_data['signal'], delta=spy1h_data['price'])                          
+col4.metric(label='SPY 15m', value=spy15m_data['signal'], delta=spy15m_data['price'])                          
+col5.metric(label='SPY 30m', value=spy30m_data['signal'], delta=spy30m_data['price'])                          
+col6.metric(label='SPY 1h', value=spy1h_data['signal'], delta=spy1h_data['price'])                          
