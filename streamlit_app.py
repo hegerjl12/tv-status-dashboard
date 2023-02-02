0799154d05261b456e9f5d30c3604953e9462208
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-02-02 13:33:23 -0800

    setup for new current price db

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from deta import Deta
 from streamlit_autorefresh import st_autorefresh
 from datetime import datetime, timedelta
-import yfinance as yf
 
 deta = Deta('b0hip04s_DyG5HST9fRrAtbUr425Q9bDNLSaLScv5')
 
@@ -12,9 +11,10 @@ spy5m_db = deta.Base('SPY5m')
 spy15m_db = deta.Base('SPY15m')
 spy30m_db = deta.Base('SPY30m')
 spy1h_db = deta.Base('SPY1h')
+spy_current_price_db = deta.Base('SPY_PRICE')
 
-stock_info = yf.Ticker('SPY').fast_info
-market_price = stock_info['regularMarketPrice']
+#market_price_data = spy_current_price_db.get('current')
+#market_price = float(market_price_data['price'])
 
 spy1m_data = spy1m_db.get('current')
 spy1m_price = float(spy1m_data['price'])
@@ -59,7 +59,7 @@ col1, col2, col3, col4, col5, col6 = st.columns(6)
 
 
 
-col1.metric(label='SPY 1m', value=spy1m_data['signal'], delta=market_price)
+col1.metric(label='SPY 1m', value=spy1m_data['signal'], delta=spy1m_price)
 col2.metric(label='SPY 3m', value=spy3m_data['signal'], delta=spy3m_price)
 col3.metric(label='SPY 5m', value=spy5m_data['signal'], delta=spy5m_price)                          
 col4.metric(label='SPY 15m', value=spy15m_data['signal'], delta=spy15m_price)                          
