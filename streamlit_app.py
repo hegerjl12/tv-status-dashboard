34b025acd9ee83e1aca1bb416df56f5c73bf5d02
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-02-03 10:07:23 -0800

    all emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,22 +26,42 @@ spy1m_delta_price = float(market_price)-spy1m_price
   
 spy3m_data = spy3m_db.get('current')
 spy3m_price = float(spy3m_data['price'])
+if spy3m_data['signal'] == 'buy':
+  spy3m_signal = '🟢'
+if spy3m_data['signal'] == 'sell':
+  spy3m_signal = '🔴'
 spy3m_delta_price = float(market_price)-spy3m_price
   
 spy5m_data = spy5m_db.get('current')
 spy5m_price = float(spy5m_data['price'])
+if spy5m_data['signal'] == 'buy':
+  spy5m_signal = '🟢'
+if spy5m_data['signal'] == 'sell':
+  spy5m_signal = '🔴'
 spy5m_delta_price = float(market_price)-spy5m_price
   
 spy15m_data = spy15m_db.get('current')
 spy15m_price = float(spy15m_data['price'])
+if spy15m_data['signal'] == 'buy':
+  spy15m_signal = '🟢'
+if spy15m_data['signal'] == 'sell':
+  spy15m_signal = '🔴'
 spy15m_delta_price = float(market_price)-spy15m_price
   
 spy30m_data = spy30m_db.get('current')
 spy30m_price = float(spy30m_data['price'])
+if spy30m_data['signal'] == 'buy':
+  spy30m_signal = '🟢'
+if spy30m_data['signal'] == 'sell':
+  spy30m_signal = '🔴'
 spy30m_delta_price = float(market_price)-spy30m_price
   
 spy1h_data = spy1h_db.get('current')
 spy1h_price = float(spy1h_data['price'])
+if spy1h_data['signal'] == 'buy':
+  spy1h_signal = '🟢'
+if spy1h_data['signal'] == 'sell':
+  spy1h_signal = '🔴'
 spy1h_delta_price = float(market_price)-spy1h_price
   
 
@@ -57,10 +77,10 @@ col1, col2, col3, col4, col5, col6 = st.columns(6)
 
 
 col1.metric(label='SPY 1m', value=spy1m_signal, delta=round(spy1m_delta_price,2))
-col2.metric(label='SPY 3m', value=spy3m_data['signal'], delta=round(spy3m_delta_price,2))
-col3.metric(label='SPY 5m', value=spy5m_data['signal'], delta=round(spy5m_delta_price,2))                          
-col4.metric(label='SPY 15m', value=spy15m_data['signal'], delta=round(spy15m_delta_price,2))                          
-col5.metric(label='SPY 30m', value=spy30m_data['signal'], delta=round(spy30m_delta_price,2))                          
-col6.metric(label='SPY 1h', value=spy1h_data['signal'], delta=round(spy1h_delta_price,2))
+col2.metric(label='SPY 3m', value=spy3m_signal, delta=round(spy3m_delta_price,2))
+col3.metric(label='SPY 5m', value=spy5m_signal, delta=round(spy5m_delta_price,2))                          
+col4.metric(label='SPY 15m', value=spy15m_signal, delta=round(spy15m_delta_price,2))                          
+col5.metric(label='SPY 30m', value=spy30m_signal, delta=round(spy30m_delta_price,2))                          
+col6.metric(label='SPY 1h', value=spy1h_signal, delta=round(spy1h_delta_price,2))
 
 count = st_autorefresh(interval=60000, limit=1000, key="fizzbuzzcounter")
