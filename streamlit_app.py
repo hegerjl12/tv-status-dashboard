830b73738d50e3f4d6650e7e3049d6dfc7389efd
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2023-02-06 12:42:23 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ from streamlit_autorefresh import st_autorefresh
 from datetime import datetime, timedelta
 import pandas as pd
 
-if "spy1m_signal" not in st.session_state:
-  st.session_state.spy1m_signal = ''
-#if "spy1m_count" not in st.session_state:
- # st.session_state.spy1m_count = 0
-
 deta = Deta('b0hip04s_DyG5HST9fRrAtbUr425Q9bDNLSaLScv5')
 
 spy1m_db = deta.Base('SPY1m')
@@ -28,18 +23,11 @@ timestamp = datetime.now()
 spy1m_data = spy1m_db.get('current')
 spy1m_price = float(spy1m_data['price'])
 spy1m_delta_price = round(float(market_price)-spy1m_price,2)
-
-#if spy1m_data['signal'] != st.session_state.spy1m_signal:
-#  st.session_state.spy1m_count = 1
-#spy1m_delta.put({'timestamp':str(timestamp), 'delta':spy1m_delta_price})
-#st.session_state.spy1m_count += 1
-
 if spy1m_data['signal'] == 'buy':
-  st.session_state.spy1m_signal = '🟢'
+  spy1m_signal = '🟢'
 if spy1m_data['signal'] == 'sell':
-  st.session_state.spy1m_signal = '🔴'
+  spy1m_signal = '🔴'
 
-  
 spy3m_data = spy3m_db.get('current')
 spy3m_price = float(spy3m_data['price'])
 if spy3m_data['signal'] == 'buy':
@@ -99,10 +87,5 @@ col4.metric(label='SPY 15m', value=spy15m_signal, delta=round(spy15m_delta_price
 col5.metric(label='SPY 30m', value=spy30m_signal, delta=round(spy30m_delta_price,2))                          
 col6.metric(label='SPY 1h', value=spy1h_signal, delta=round(spy1h_delta_price,2))
 
-#spy1m_delta_dump = spy1m_delta.fetch()
-#st.write(spy1m_delta_dump.items)
-
-#spy1m_delta_df = pd.DataFrame({'spy1m_delta':[spy1m_delta_price]})
-#st.bar_chart(spy1m_delta_price)
 
 count = st_autorefresh(interval=60000, limit=1000, key="fizzbuzzcounter")
